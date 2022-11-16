348f02e9e69d52fc1092ebcbcb7f32c9044a2459
Alex Iadicicco <alex@aloft.us>
2022-11-16 13:26:17 -0800

    Change new channel notify syntax

--- utils.js
+++ utils.js
@@ -1,46 +1,63 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const micromark = require("micromark");
+const htmlToMrkdwn = require("html-to-mrkdwn");
 
 const { WebClient } = require("@slack/web-api");
 const slackClient = new WebClient(core.getInput("slack_bot_token"));
 
 const channelId = core.getInput("slack_channel_id");
-const prReviewsChannelId = 'C04B4TN6UHJ'; // #tech-prs
+const prReviewsChannelId = "C04B4TN6UHJ"; // #tech-prs
 
 const githubToSlackName = (github) => {
   const users = JSON.parse(core.getInput("slack_users"));
-  return `<@${
-    users.find((user) => user["github_username"] === github)["slack_id"]
-  }>`;
+  const user = users.find((user) => user["github_username"] === github);
+  return user === undefined ? `*${github}*` : `<@${user["slack_id"]}>`;
 };
+
+const markdownToMrkdwn = (markdown) => htmlToMrkdwn(micromark(markdown));
+const mrkdwnQuote = (mrkdwn) =>
+  mrkdwn
+    .split("\n")
+    .map((s) => `>${s}`)
+    .join("\n");
 
 module.exports = {
   handleOpen: async () => {
     console.log("handling open");
     const { pull_request, sender } = github.context.payload;
-    const prReviewsMessage = await slackClient.chat.postMessage({
-      channel: prReviewsChannelId,
-      text: `<${pull_request._links.html.href}|*${pull_request.title}*>`,
-      // author and reviewers will be included in link preview
-      unfurl_links: true,
-    });
-    if (!prReviewsMessage.ok) {
-      throw new Error('Failed to send slack message');
-    }
+
+    const author = githubToSlackName(sender.login);
     const reviewers = pull_request.requested_reviewers.map((reviewer) =>
       githubToSlackName(reviewer.login)
     );
-    if (reviewers.length === 0) {
-      return;
-    }
-    const author = githubToSlackName(sender.login);
-    const PR = `<${pull_request._links.html.href}|*${pull_request.title}*>`;
-    const slackMessage = await slackClient.chat.postMessage({
-      channel: channelId,
-      text: `${reviewers.join(",")}, ${author} requested your review on ${PR}`,
+    const prTitleLink = `<${pull_request._links.html.href}|*${pull_request.title}*>`;
+
+    const bodySection = pull_request.body
+      ? `\n${mrkdwnQuote(markdownToMrkdwn(pull_request.body))}`
+      : "";
+    const reviewersSection =
+      reviewers.length > 0 ? `\n*Reviewers:* ${reviewers.join(", ")}` : "";
+
+    const prReviewsMessage = await slackClient.chat.postMessage({
+      channel: prReviewsChannelId,
+      text: `${author} opened ${prTitleLink}${reviewersSection}${bodySection}`,
+      unfurl_links: false,
     });
-    if (!slackMessage.ok) {
+    if (!prReviewsMessage.ok) {
       throw new Error("Failed to send slack message");
+    }
+
+    if (reviewers.length !== 0) {
+      const slackMessage = await slackClient.chat.postMessage({
+        channel: channelId,
+        text: `${reviewers.join(
+          ","
+        )}, ${author} requested your review on ${prTitleLink}`,
+      });
+      if (!slackMessage.ok) {
+        throw new Error("Failed to send slack message");
+      }
     }
   },
 
